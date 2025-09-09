e4382c159abef357dc7d3f4b6fd4fad76f3be589
HannaXTX <hannakaibni@hotmail.com>
2025-09-09 18:44:27 +0300

    Configured Colors for project theme + animation + logo

--- app/src/main/java/edu/birzeit/project1/WelcomeActivity.java
+++ app/src/main/java/edu/birzeit/project1/WelcomeActivity.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -16,13 +21,47 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        Button connectButton = findViewById(R.id.connect_button);
 
-        connectButton.setOnClickListener(new View.OnClickListener() {
+        Button btnConnect = findViewById(R.id.connect_button);
+        ImageView ivLogo = findViewById(R.id.logo);
+        LinearLayout llRoot = findViewById(R.id.root_layout);
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        Animation fadeInQuick = AnimationUtils.loadAnimation(this, R.anim.fade_in_quick);
+
+        TextView tvTitle = findViewById(R.id.intro_title);
+        TextView tvDescription = findViewById(R.id.intro_description);
+
+        btnConnect.setVisibility(View.INVISIBLE);
+        tvTitle.setVisibility(View.INVISIBLE);
+        tvDescription.setVisibility(View.INVISIBLE);
+
+        fadeIn.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+//                ivLogo.post(() -> llRoot.removeView(ivLogo));
+                btnConnect.startAnimation(fadeInQuick);
+                tvTitle.startAnimation(fadeInQuick);
+                tvDescription.startAnimation(fadeInQuick);
+                btnConnect.setVisibility(View.VISIBLE);
+                tvTitle.setVisibility(View.VISIBLE);
+                tvDescription.setVisibility(View.VISIBLE);
+
+            }
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        ivLogo.startAnimation(fadeIn);
+        btnConnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(WelcomeActivity.this, "Connecting to library...", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 }

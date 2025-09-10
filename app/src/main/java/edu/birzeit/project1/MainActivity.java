b75ef1ed209ab185b90db5d978693a4f86a2abc0
HannaXTX <hannakaibni@hotmail.com>
2025-09-10 21:49:12 +0300

    expanded on navlist

--- app/src/main/java/edu/birzeit/project1/MainActivity.java
+++ app/src/main/java/edu/birzeit/project1/MainActivity.java
@@ -1,29 +1,127 @@
 package edu.birzeit.project1;
 
+import static android.app.PendingIntent.getActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        TextView tvTest = new TextView(this);
-        tvTest.setText("Hanna");
-        navigationView.addHeaderView(tvTest);
+
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        Button btn_catalog = findViewById(R.id.btn_catalog);
+        Button btn_dashboard = findViewById(R.id.btn_dashboard);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, new DashboardFragment()).commit();
+        navigationView.setCheckedItem(R.id.nav_dashboard);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.nav_dashboard:
+                        selectedFragment = new DashboardFragment();
+                        break;
+                    case R.id.nav_borrowed_books:
+                        selectedFragment = new BorrowedBooksFragment();
+                        break;
+                    case R.id.nav_reading_list:
+                        selectedFragment = new ReadingListFragment();
+                        break;
+                    case R.id.nav_profile:
+                        selectedFragment = new ProfileManagementFragment();
+                        break;
+                    case R.id.nav_new_arrivals:
+                        selectedFragment = new NewArrivalsFragment();
+                        break;
+                    case R.id.nav_library_info:
+                        selectedFragment = new LibraryInfoFragment();
+                        break;
+                    case R.id.nav_logout:
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setTitle("Logout");
+                        builder.setMessage("sure you want to Logout?")
+                                .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                })
+                                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+
+                        builder.show();
+                        break;
+
+
+                }
+
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragmentContainerView, selectedFragment)
+                            .commit();
+                }
+
+                item.setChecked(true);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+        btn_catalog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment selectedFragment = null;
+                selectedFragment = new BookCatalogFragment();
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragmentContainerView, selectedFragment)
+                            .commit();
+                }
+            }
+        });
+        btn_dashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment selectedFragment = null;
+                selectedFragment = new DashboardFragment();
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragmentContainerView, selectedFragment)
+                            .commit();
+                }
+            }
+        });
+
+
     }
-
-
 }
